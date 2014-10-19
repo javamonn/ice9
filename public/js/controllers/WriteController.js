f68dd1133c19d5f18df548eea3b84eafe8dee13b
Daniel Ramirez <javamonn@gmail.com>
2014-10-18 19:55:49 -0500

    writer edits, begin work on preview

--- public/js/controllers/WriteController.js
+++ public/js/controllers/WriteController.js
@@ -3,8 +3,29 @@ angular.module('WriteCtrl', []).controller('WriteController', function($scope, L
 	// secret returned on successful auth, validated server side when attempting to post.
 	// This ensures that even if someone sets the authenticated var on scope, they won't
 	// be able to post with the secret from the server.
-	var secret = "";
+	var secret;
+	var authenticated;
+	var preview;
 
+	init();
+
+	function init() {
+		secret = "";
+		authenticated = false;
+		preview = false;
+	}
+
+	$scope.showAuthenticate = function () {
+		return !authenticated;
+	}
+
+	$scope.showPreview = function () {
+		return authenticated && preview; 
+	}
+
+	$scope.showEdit = function () {
+		return authenticated && !preview;
+	}
 
 	/**
 	 * Attempt to authenticate. If authenticated, store secret and reveal the writer. 
@@ -16,13 +37,22 @@ angular.module('WriteCtrl', []).controller('WriteController', function($scope, L
 		}, function (res) {
 			if(res.status != 401) {
 				// show the write view
-				$scope.authenticated = true;
-				console.log(res);
+				authenticated = true;
+				$scope.authKey = "";
 			}
-			$scope.authKey = "";
-		})
+		});
 	};
 
+	/**
+	 * Display a preview showing the curent information in a card and the post.
+	 */
+	 $scope.preview = function() {
+	 	preview = true;
+	 }
+
+	/**
+	 * Save post.
+	 */
 	$scope.post = function() {
 		var post = new Post({
 			title: $scope.postTitle,
