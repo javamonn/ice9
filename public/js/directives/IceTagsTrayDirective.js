05abf01a59c0fbe67b7369b761d494d72f99e9f1
Daniel Ramirez <javamonn@gmail.com>
2014-10-23 00:26:25 -0500

    tray icon animation

--- public/js/directives/IceTagsTrayDirective.js
+++ public/js/directives/IceTagsTrayDirective.js
@@ -9,18 +9,34 @@ angular.module('IceTagsTrayDirective', [])
 			},
 			link: function (scope, elem, attrs) {
 				console.log(elem);
-				var tagsDiv = elem.find('.ice-tags-icon-container');
+				var tagsDiv = elem.find('.ice-label-icon');
 				console.log(tagsDiv);
 				tagsDiv.bind('mouseenter', function () {
 					$animate.addClass(tagsDiv, 'tray-expanded');
-					console.log("add");
 					scope.$apply();
 				});
 				tagsDiv.bind('mouseleave', function () {
 					$animate.removeClass(tagsDiv, 'tray-expanded');
-					console.log("remove");
 					scope.$apply();
 				});
 			}
+		}
+	})
+	/**
+	 * JS animations to expand and contract the tray, the icon rotation is handled in CSS. 
+	 */
+	.animation('.tray-expanded', function () {
+		var expandTray = function (elem, className, done) {
+
+		}
+
+		var contractTray = function (elem, className, done) {
+
+		}
+
+
+		return {
+			addClass: expandTray,
+			removeClass: contractTray
 		}
 	});
