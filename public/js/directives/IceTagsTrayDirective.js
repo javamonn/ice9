4c6e20d5975ac402e08dd3fd870d51c5a9a6f5b8
Daniel Ramirez <javamonn@gmail.com>
2014-10-23 02:41:46 -0500

    animation work

--- public/js/directives/IceTagsTrayDirective.js
+++ public/js/directives/IceTagsTrayDirective.js
@@ -5,33 +5,61 @@ angular.module('IceTagsTrayDirective', [])
 			replace: true,
 			templateUrl: 'templates/IceTagsTray.html',
 			scope: {
-				tags: '@'
+				tags: '='
 			},
 			link: function (scope, elem, attrs) {
-				console.log(elem);
+				scope.trayExpanded = false;
 				var tagsDiv = elem.find('.ice-label-icon');
-				console.log(tagsDiv);
 				tagsDiv.bind('mouseenter', function () {
+					scope.trayExpanded = true;
 					$animate.addClass(tagsDiv, 'tray-expanded');
 					scope.$apply();
 				});
 				tagsDiv.bind('mouseleave', function () {
+					scope.trayExpanded = false;
 					$animate.removeClass(tagsDiv, 'tray-expanded');
 					scope.$apply();
 				});
 			}
 		}
 	})
+
 	/**
 	 * JS animations to expand and contract the tray, the icon rotation is handled in CSS. 
 	 */
-	.animation('.tray-expanded', function () {
+	.animation('.tags-tray', function () {
 		var expandTray = function (elem, className, done) {
+			var TAG_PADDING = 5;
+			var EDGE_PADDING = 2.5;
+			var totalWidth = 0;
+
+			// measure the width of the tag element divs
+			var tags = elem.children('span');
+			console.log(tags);
+			tags.each(function(index, element) {
+				totalWidth += parseInt($(element).css('width'));
+			});
+
+			// add the padding width between tag elements
+			totalWidth += ((tags.length - 1) * TAG_PADDING);
+
+			// add edge padding
+			totalWidth += (EDGE_PADDING * 2);
+
+			console.log(totalWidth);
+			// animate the tray expansion
+			elem.animate({
+				backgroundColor: 'rgba(0, 0, 0, .12)',
+				width: totalWidth
+			}, 500);
 
 		}
 
 		var contractTray = function (elem, className, done) {
-
+			elem.animate({
+				backgroundColor: '#FFF',
+				width: 0
+			}, 500);
 		}
 
 
