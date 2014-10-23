f4eb8d00b67de4622bfedbb57bd1a7dd4dd8ba26
Daniel Ramirez <javamonn@gmail.com>
2014-10-22 21:27:47 -0500

    directive animation scaffolding

--- public/js/directives/IceTagsTrayDirective.js
+++ public/js/directives/IceTagsTrayDirective.js
@@ -1,10 +1,26 @@
-angular.module('IceTagsTrayDirective', []).directive('iceTagsTray', function() {
-	return {
-		restrict: 'E',
-		replace: true,
-		templateUrl: 'templates/IceTagsTray.html',
-		scope: {
-			tags: '@'
+angular.module('IceTagsTrayDirective', [])
+	.directive('iceTagsTray', function ($animate) {
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'templates/IceTagsTray.html',
+			scope: {
+				tags: '@'
+			},
+			link: function (scope, elem, attrs) {
+				console.log(elem);
+				var tagsDiv = elem.find('.ice-tags-icon-container');
+				console.log(tagsDiv);
+				tagsDiv.bind('mouseenter', function () {
+					$animate.addClass(tagsDiv, 'tray-expanded');
+					console.log("add");
+					scope.$apply();
+				});
+				tagsDiv.bind('mouseleave', function () {
+					$animate.removeClass(tagsDiv, 'tray-expanded');
+					console.log("remove");
+					scope.$apply();
+				});
+			}
 		}
-	}
-});
+	});
