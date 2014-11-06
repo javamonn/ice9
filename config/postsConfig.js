7d712924f5713d4a8dfa5fe441d7e947b6500ada
Daniel Ramirez <javamonn@gmail.com>
2014-11-05 22:13:49 -0600

    added conclusion

--- config/postsConfig.js
+++ config/postsConfig.js
@@ -1,7 +1,7 @@
 module.exports = [
 	{
 		title: "The Material Monk",
-		subtitle: "Material Design as implemented in the music of a Jazz master",
+		subtitle: "Material Design in the music of a Jazz master",
 		imageUrl: "assets/Thelonious-Monk.jpg",
 		tags: ["Material Design", "Design", "Monk", "Jazz", "Bebop"],
 		date: "11/2/2014",
