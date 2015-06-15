7226e519fa9dbdf05feb1891c390ac6f1246ceb7
Daniel Ramirez <javamonn@gmail.com>
2015-06-14 21:12:18 -0400

    begin writing angular-redux intro

--- icenine/config/postsConfig.js
+++ icenine/config/postsConfig.js
@@ -3,8 +3,16 @@ module.exports = [
     title: "The Material Monk",
     subtitle: "Material Design in the music of a Jazz master",
     imageUrl: "assets/Thelonious-Monk.jpg",
+    templateUrl: 'Material-Monk',
     tags: ["Material Design", "Design", "Monk", "Jazz", "Bebop"],
-    date: "11/2/2014",
-    templateUrl: 'Material-Monk'
+    date: "11/2/2014"
+  },
+  {
+    title: "Angular Redux",
+    subtitle: "Modern JS structure and techniques as applied to Angular",
+    imageUrl: "",
+    templateUrl: "",
+    tags: ["Angular", "Reflux", "Immutable", "PouchDB"] ,
+    date: ""
   }
 ];
